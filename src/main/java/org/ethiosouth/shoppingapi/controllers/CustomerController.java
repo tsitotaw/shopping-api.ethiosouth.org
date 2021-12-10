4b24553ab0f4eee6326edf9425d435a15c4e4b86
Tamirat Fisseha <tamiratfissehasitotaw@gmail.com>
2021-12-10 16:39:58 -0600

    added controllers

---
+++ src/main/java/org/ethiosouth/shoppingapi/controllers/CustomerController.java
@@ -0,0 +1,42 @@
+package org.ethiosouth.shoppingapi.controllers;
+
+import org.ethiosouth.shoppingapi.domain.Customer;
+import org.ethiosouth.shoppingapi.services.implementation.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+@CrossOrigin
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping
+    public List<Customer> getAll(){
+        return this.customerService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Customer getAll(@PathVariable Long id){
+        return this.customerService.findById(id);
+    }
+
+    @PostMapping
+    public Customer save(@RequestBody Customer customer){
+        return this.customerService.save(customer);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody Customer customer, @PathVariable Long id){
+        this.customerService.update(customer, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        this.customerService.delete(id);
+    }
+}
