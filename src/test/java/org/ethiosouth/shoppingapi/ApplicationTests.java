415a304fa44e95f68478029f1b4ce0afd0eb78ed
Tamirat Fisseha <tamiratfissehasitotaw@gmail.com>
2021-12-10 11:07:42 -0600

    initial commit

---
+++ src/test/java/org/ethiosouth/shoppingapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ethiosouth.shoppingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
